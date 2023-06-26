a0aec3d08b4468674d32bc6e387993e63b0adefc
Brock Klein <brock.klein@circumference.co>
2023-06-26 08:29:39 -0400

    Update selected month

--- src/get-selected-month.ts
+++ src/get-selected-month.ts
@@ -1,7 +1,7 @@
 import dayjs from 'dayjs'
 
 export const getSelectedMonth = () => {
-	const selectedMonth = document.getElementById('ember153')
+	const selectedMonth = document.querySelector('[data-test="month-selector"')
 	if (selectedMonth && selectedMonth.textContent) {
 		return dayjs(selectedMonth.textContent, 'MMM YYYY')
 	}
